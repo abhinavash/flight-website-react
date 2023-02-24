9c7eb0baa218d82c6d8e2640cdb27f4f14b293e9
Abhinav <abhinava@observancegroup.com>
2023-02-24 18:24:28 +0530

    created support section

--- src/Components/Support/Support.jsx
+++ src/Components/Support/Support.jsx
@@ -2,7 +2,52 @@ import React from 'react'
 
 const Support = () => {
   return (
-    <div>Support</div>
+    <div className='support container section'>
+      <div className="sectionContainer">
+        <div className="titlesDiv">
+          <small>travel support</small>
+          <h2>Plan your travel with confidence</h2>
+          <p>Find help with booking and travel plans, see what to except along with journey!</p>
+        </div>
+
+        <div className="infoDiv grid">
+          <div className="textDiv grid">
+
+            <div className="singleInfo  ">
+              <span className="number ">01</span>
+              <h4>Travel requirements for Dubai</h4>
+              <p>
+                Find help with booking and travel plans, see what to expect
+                along the journey to your favourite destinations!
+              </p>
+            </div>
+
+            <div className="singleInfo ">
+              <span className="number colorOne">02</span>
+              <h4>Travel requirements for Dubai</h4>
+              <p>
+                Find help with booking and travel plans, see what to expect
+                along the journey to your favourite destinations!
+              </p>
+            </div>
+
+            <div className="singleInfo ">
+              <span className="number colorTwo">03</span>
+              <h4>Travel requirements for Dubai</h4>
+              <p>
+                Find help with booking and travel plans, see what to expect
+                along the journey to your favourite destinations!
+              </p>
+            </div>
+
+          </div>
+          
+          <div className="imgDiv">
+            <img src=""  alt=""/>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
